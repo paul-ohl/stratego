2d5a50417e73dc89fd7caffb05ffa56f5a62822b
Paul OHL <paul.lv.ohl@gmail.com>
2023-12-13 13:41:54 +0100

    feat: added the socket logic

--- api_hint/src/app.module.ts
+++ api_hint/src/app.module.ts
@@ -5,24 +5,26 @@ import { ConfigModule } from '@nestjs/config';
 import { HintModule } from './hint/hint.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GamesModule } from './games/games.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({}),
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: process.env.DB_HOST || 'localhost',
-      port: +process.env.DB_PORT || 3307,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'stratego',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    HintModule,
-    GamesModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({}),
+		TypeOrmModule.forRoot({
+			type: 'mariadb',
+			host: process.env.DB_HOST || 'localhost',
+			port: +process.env.DB_PORT || 3307,
+			username: process.env.DB_USERNAME || 'root',
+			password: process.env.DB_PASSWORD || 'root',
+			database: process.env.DB_NAME || 'stratego',
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
+		HintModule,
+		GamesModule,
+		EventsModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
